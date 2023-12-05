b5a47e0fbc4c3cfad078cd00935da45f73ed4b28
Jonathan Pereira <jonathan14willian@gmail.com>
2023-12-05 18:08:10 -0300

    Update .gitignore file

---
+++ negociate-app/dist/js/model/negociate.js
@@ -0,0 +1,27 @@
+export class Negociate {
+    constructor(data, quantity, value) {
+        this._data = data;
+        this._quantity = quantity;
+        this._value = value;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantity() {
+        return this._quantity;
+    }
+    get value() {
+        return this._value;
+    }
+    get volume() {
+        return this._quantity * this._value;
+    }
+    toString() {
+        return `
+            Data: ${this.data},
+            Quantidade: ${this.quantity},
+            Valor: ${this.value},
+            Volume: ${this.volume}
+        `;
+    }
+}
