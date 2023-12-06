f88f307b15b142b56d603a2999918f16c5f09c8e
Jonathan Pereira <jonathan14willian@gmail.com>
2023-12-06 14:42:59 -0300

    Refactor code structure and remove unnecessary style tag

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,12 @@
 
 <script lang="ts">
-import Banner from "./components/Banner.vue";
+  import Banner from "./components/Banner.vue";
 
-export default {
-  components: { Banner },
-};
+  export default {
+    components: { Banner }
+  }
 </script>
 
 <template>
   <Banner />
 </template>
-
-<style scoped>
-</style>
